38874f0462acce0a9ea7bd7c9f5afe2aaabac502
ggrove87 <gwg1387@gmail.com>
2022-06-07 08:54:28 -0500

    Updated Portfolio Page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,49 @@
 import React from 'react';
+import project2 from "../../assets/images/Project_2_ScreenShot.PNG";
+import eCommerce from "../../assets/images/E-Commerce_Back_End.PNG";
+import noteTaker from "../../assets/images/NoteTaker.PNG";
+import teamProfileGenerator from "../../assets/images/TeamProfileGenerator.PNG";
 
 export default function Home() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    
+      <section id="projects-container">
+            <h2>Images of Gene's Applications</h2>
+
+      
+            <div id="project1" class="myProjects">
+                <h2 class="card-header">Art Institute Events Project</h2>
+                <a class="card-header" href="https://github.com/NBFxSuccess/ArtInstituteProject" target="_blank">Associated github repository</a>
+                <a href="https://tranquil-island-74133.herokuapp.com/" target="_blank"><img id="coolWebsite" src={project2}
+                        alt="ArtInstituteProject"/></a>
+            </div>
+
+           
+            <div class="myProjects">
+                <h2 class="card-header">E-Commerce Back End</h2>
+                <a class="card-header" href="https://github.com/ggrove87/E-Commerce_Back_End" target="_blank">Associated github repository</a>
+                <a href="https://drive.google.com/file/d/1IkFzg_YDaF5bfpmXnwmzi0bgOvm4tS_Z/view"
+                    target="_blank"><img class="project-photos" src={eCommerce} alt="Cool_Cave"/></a>
+            </div>
+
+            <div class="myProjects">
+                <h2 class="card-header">NoteTaker</h2>
+                <a class="card-header" href="https://github.com/ggrove87/NoteTaker" target="_blank">Associated github repository</a>
+                <a href="https://shrouded-sierra-86347.herokuapp.com/" target="_blank"><img class="project-photos"
+                        src={noteTaker} alt="Sampson"/></a>
+
+            </div>
+            <div class="myProjects">
+                <h2 class="card-header">Team Profile Generator</h2>
+                <a class="card-header" href="https://github.com/ggrove87/Team-Profile-Generator" target="_blank">Associated github repository</a>
+                <a href="https://drive.google.com/file/d/1P5v3u2HJ5x-z4WC1kU4-KJJC91B_WHEw/view" target="_blank"><img class="project-photos" src={teamProfileGenerator}
+                        alt="Good_Food"/></a>
+            </div>
+        </section>
+   
+     
     </div>
   );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import resume from '../../assets/Gene-Grove-Resume.pdf'
 
 export default function Resume() {
   return (
     <div>
       <h1>Resume Page</h1>
-      <a href="../../../assets/Gene Grove Resume.pdf" target="_blank"><p>Link to Resume download</p></a>
+      <a href={resume} target="_blank"><p>Link to Resume download</p></a>
     </div>
   );
 }
