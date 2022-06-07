374ba19a9c64821c8de42ba64d49c7c4421d0bd9
ggrove87 <gwg1387@gmail.com>
2022-06-07 09:25:53 -0500

    Added place holder Style sheet and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+<footer>
+        <h2>Made by Gene Grove</h2>
+        <p>
+            &copy; Wish I was cool enough to copyright stuff yet 😋
+        </p>
+    </footer>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,32 +13,32 @@ export default function Home() {
             <h2>Images of Gene's Applications</h2>
 
       
-            <div id="project1" class="myProjects">
-                <h2 class="card-header">Art Institute Events Project</h2>
-                <a class="card-header" href="https://github.com/NBFxSuccess/ArtInstituteProject" target="_blank">Associated github repository</a>
+            <div id="project1" className="col-sm-10 col-md-4 myProjects">
+                <h2 className="card-header">Art Institute Events Project</h2>
+                <a className="card-header" href="https://github.com/NBFxSuccess/ArtInstituteProject" target="_blank">Associated github repository</a>
                 <a href="https://tranquil-island-74133.herokuapp.com/" target="_blank"><img id="coolWebsite" src={project2}
                         alt="ArtInstituteProject"/></a>
             </div>
 
            
-            <div class="myProjects">
-                <h2 class="card-header">E-Commerce Back End</h2>
-                <a class="card-header" href="https://github.com/ggrove87/E-Commerce_Back_End" target="_blank">Associated github repository</a>
+            <div className="col-sm-10 col-md-4 myProjects">
+                <h2 className="card-header">E-Commerce Back End</h2>
+                <a className="card-header" href="https://github.com/ggrove87/E-Commerce_Back_End" target="_blank">Associated github repository</a>
                 <a href="https://drive.google.com/file/d/1IkFzg_YDaF5bfpmXnwmzi0bgOvm4tS_Z/view"
-                    target="_blank"><img class="project-photos" src={eCommerce} alt="Cool_Cave"/></a>
+                    target="_blank"><img className="project-photos" src={eCommerce} alt="Cool_Cave"/></a>
             </div>
 
-            <div class="myProjects">
-                <h2 class="card-header">NoteTaker</h2>
-                <a class="card-header" href="https://github.com/ggrove87/NoteTaker" target="_blank">Associated github repository</a>
-                <a href="https://shrouded-sierra-86347.herokuapp.com/" target="_blank"><img class="project-photos"
+            <div className="col-sm-10 col-md-4 myProjects">
+                <h2 className="card-header">NoteTaker</h2>
+                <a className="card-header" href="https://github.com/ggrove87/NoteTaker" target="_blank">Associated github repository</a>
+                <a href="https://shrouded-sierra-86347.herokuapp.com/" target="_blank"><img className="project-photos"
                         src={noteTaker} alt="Sampson"/></a>
 
             </div>
-            <div class="myProjects">
-                <h2 class="card-header">Team Profile Generator</h2>
-                <a class="card-header" href="https://github.com/ggrove87/Team-Profile-Generator" target="_blank">Associated github repository</a>
-                <a href="https://drive.google.com/file/d/1P5v3u2HJ5x-z4WC1kU4-KJJC91B_WHEw/view" target="_blank"><img class="project-photos" src={teamProfileGenerator}
+            <div className="col-sm-10 col-md-4 myProjects">
+                <h2 className="card-header">Team Profile Generator</h2>
+                <a className="col-sm-10 col-md-4 card-header" href="https://github.com/ggrove87/Team-Profile-Generator" target="_blank">Associated github repository</a>
+                <a href="https://drive.google.com/file/d/1P5v3u2HJ5x-z4WC1kU4-KJJC91B_WHEw/view" target="_blank"><img className="project-photos" src={teamProfileGenerator}
                         alt="Good_Food"/></a>
             </div>
         </section>
