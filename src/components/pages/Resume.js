c45190d6652c65dd88f570335836b721fe05fe37
ggrove87 <gwg1387@gmail.com>
2022-06-09 10:08:58 -0500

    Updated Resume Page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,6 +6,16 @@ export default function Resume() {
     <div>
       <h1>Resume Page</h1>
       <a href={resume} target="_blank"><p>Link to Resume download</p></a>
+      <h2>Technical Skills</h2>
+      <ul>
+        <li>SQL</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Mongo</li>
+      </ul>
+
     </div>
   );
 }
